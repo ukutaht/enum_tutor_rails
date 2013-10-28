59dcc99120c464a7ced0f7ac264c2507d98b342c
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 15:27:04 -0500

    sessions and omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
-
+  
   def sign_in
-
+    redirect_to '/auth/dbc'
   end
 
   def sign_out
+    session.clear
+    redirect_to root_path
+  end
 
+  def auth
+  user_attributes = request.env['omniauth.auth'].info
+  session[:user_attributes] = user_attributes
+  token = request.env['omniauth.auth'].credentials
+  user = User.from_auth(user_attributes)
+  session[:oauth_token] = token_as_hash(token)
+  
+  redirect_to root_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ EnumTutor::Application.routes.draw do
 
   get '/signin', :to => 'sessions#sign_in'
   post 'signout', :to => 'sessions#sign_out'
+  get '/auth/:provider/callback', :to => 'sessions#auth'
 
   root :to => 'pages#welcome'  
 end
