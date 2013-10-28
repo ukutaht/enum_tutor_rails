fb04ddd670ec94732639d3cffe598d78ad9c84cf
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 15:39:39 -0500

    deleted old files

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,15 @@ class User < ActiveRecord::Base
   
   validates :socrates_id, presence: true
   validates :email, presence: true
-  
+
+  def self.from_auth(user_attributes)
+    where(user_attributes.slice(:id, :name)).first_or_initialize.tap do |user|
+      user.socrates_id = user_attributes.id
+      user.name = user_attributes.name
+      user.email = user_attributes.email
+      user.gravatar = user_attributes.gravatar
+      user.save!
+    end
+  end
+
 end
