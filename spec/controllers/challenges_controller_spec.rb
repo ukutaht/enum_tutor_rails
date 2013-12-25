ca95c16ad1529c45781674a8fa27964c486f5c8b
heruku <uku.taht@gmail.com>
2013-12-25 20:35:36 +0200

    delete local safe ruby implementation, tests for ChallengesController#attempt

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -29,4 +29,60 @@ describe ChallengesController do
       assigns(:challenge).should eq @challenge
     end
   end
+
+
+  describe 'POST attempt' do
+    before do
+      SafeRuby.stub(:eval)
+      post :attempt, id: @challenge.id, attempt: FactoryGirl.attributes_for(:attempt)
+    end
+
+    it 'assings all challenges' do
+      assigns(:challenges).should include(@challenge)
+    end
+
+    it 'assigns current challenge' do
+      assigns(:challenge).should eq @challenge
+    end
+
+    it 'creates attempt' do
+      expect{ post :attempt, id: @challenge.id, attempt: FactoryGirl.attributes_for(:attempt) }.to \
+       change { Attempt.count }.by(1)
+    end
+
+    it 'associates attempt with challenge' do
+      expect{ post :attempt, id: @challenge.id, attempt: FactoryGirl.attributes_for(:attempt) }.to \
+       change { @challenge.attempts.count }.by(1)
+    end
+
+    it 'renders show' do
+      response.should render_template(:show)
+    end
+
+    context 'good attempt' do
+      before do
+        attempt = FactoryGirl.attributes_for(:attempt)
+        SafeRuby.should_receive(:eval).with(attempt[:attempt_text]).and_return([2,3,4])
+        SafeRuby.should_receive(:eval).with(@challenge.expected_output).and_return([2,3,4])
+        post :attempt, id: @challenge.id, attempt: attempt
+      end
+
+      it 'sets attempt#passed to true' do
+        assigns(:attempt).passed.should be_true
+      end
+    end
+
+    context 'bad attempt' do
+      before do
+        attempt = FactoryGirl.attributes_for(:attempt)
+        SafeRuby.should_receive(:eval).with(attempt[:attempt_text]).and_return([1,2,3])
+        SafeRuby.should_receive(:eval).with(@challenge.expected_output).and_return([2,3,4])
+        post :attempt, id: @challenge.id, attempt: attempt
+      end
+
+      it 'sets attempt#passed to false' do
+        assigns(:attempt).passed.should be_false
+      end
+    end
+  end
 end
